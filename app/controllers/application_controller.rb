9039170fbb5cf0ba5e9d312e101152d7cd499b7b
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-03-21 12:43:16 +0900

    sign_up開く

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :name])
   end
   # devise_parameter_sanitizer.permit(:deviseの処理名, keys: [:許可したいキー（カラム名）]
 end
