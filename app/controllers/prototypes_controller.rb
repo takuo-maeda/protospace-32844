bfd544ef1654c0e3828e3e4458dcf4fff706250f
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-03-22 23:42:22 +0900

    3term途中

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Room.room.new(prototype_params)
+       if @prototype.save?
+          redirect_to render_to root_path
+       else
+        render :new
+       end
+  end
+
+  private
+
+  def prototype_params
+    params.reqire(:prototype).permit(:name, :image).merge(user_id: current_user.id)
   end
 end
