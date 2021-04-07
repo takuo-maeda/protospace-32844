3ae78a14141077e01f7f8b6ae726f7245b60460f
takuo-maeda <cafeacb@olivesheep34.sakura.ne.jp>
2021-04-08 00:26:49 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @prototypes = @user.prototypes
+    # binding.pry
+    # @prototypes = @user.prototypes
+    # @prototypes = Prototype.where(user_id: params[:id])
+    @prototypes = Prototype.where(user_id: @user.id)
   end
 end
